8898441bc573edc52d95253530ad6d05f9f44f3d
Dan Allan <dallan@bnl.gov>
2020-10-19 11:23:44 -0400

    Apply black

--- bluesky_live/tests/test_run_builder.py
+++ bluesky_live/tests/test_run_builder.py
@@ -9,19 +9,18 @@ def test_minimal_run():
     builder = RunBuilder()
     run = builder.get_run()
     builder.close()
-    assert run.metadata['start'] is not None
-    assert run.metadata['stop'] is not None
+    assert run.metadata["start"] is not None
+    assert run.metadata["stop"] is not None
 
 
 def test_minimal_run_using_context_manager():
     with RunBuilder() as builder:
         run = builder.get_run()
-    assert run.metadata['start'] is not None
-    assert run.metadata['stop'] is not None
+    assert run.metadata["start"] is not None
+    assert run.metadata["stop"] is not None
 
 
 def test_error_within_context_manager():
-
     class CustomException(Exception):
         ...
 
@@ -30,8 +29,8 @@ def test_error_within_context_manager():
         with RunBuilder() as builder:
             run = builder.get_run()
             raise err
-    assert run.metadata['stop']['exit_status'] == 'fail'
-    assert run.metadata['stop']['reason'] == repr(err)
+    assert run.metadata["stop"]["exit_status"] == "fail"
+    assert run.metadata["stop"]["reason"] == repr(err)
 
 
 def test_custom_metadata_in_start_doc():
@@ -63,8 +62,8 @@ def test_add_stream_from_data_keys():
             "primary",
             data_keys={
                 "x": {"source": "made up", "dtype": "number", "shape": []},
-                "y": {"source": "made up", "dtype": "number", "shape": []}
-            }
+                "y": {"source": "made up", "dtype": "number", "shape": []},
+            },
         )
     run = builder.get_run()
     run.primary
@@ -76,19 +75,24 @@ def test_add_data_from_various_structures():
             "primary",
             data_keys={
                 "x": {"source": "made up", "dtype": "number", "shape": []},
-                "y": {"source": "made up", "dtype": "number", "shape": []}
-            }
+                "y": {"source": "made up", "dtype": "number", "shape": []},
+            },
         )
-        run = builder.get_run()
-        builder.add_data("primary", {'x': [1, 2, 3], 'y': [10, 20, 30]})
-        builder.add_data("primary", pandas.DataFrame({'x': [4, 5, 6], 'y': [40, 50, 60]}))
-        builder.add_data("primary", xarray.Dataset(
-            {'x': xarray.DataArray([7, 8, 9]), 'y': xarray.DataArray([70, 80, 90])}))
+        builder.add_data("primary", {"x": [1, 2, 3], "y": [10, 20, 30]})
+        builder.add_data(
+            "primary", pandas.DataFrame({"x": [4, 5, 6], "y": [40, 50, 60]})
+        )
+        builder.add_data(
+            "primary",
+            xarray.Dataset(
+                {"x": xarray.DataArray([7, 8, 9]), "y": xarray.DataArray([70, 80, 90])}
+            ),
+        )
 
 
 def test_add_stream_from_data():
     with RunBuilder() as builder:
-        builder.add_stream("baseline", data={'a': [1, 1, 2, 3, 5, 8]})
+        builder.add_stream("baseline", data={"a": [1, 1, 2, 3, 5, 8]})
     run = builder.get_run()
     run.baseline.read()
 
@@ -99,26 +103,24 @@ def test_add_stream_from_data_keys_with_extras():
             "primary",
             data_keys={
                 "x": {"source": "made up", "dtype": "number", "shape": []},
-                "y": {"source": "made up", "dtype": "number", "shape": []}
+                "y": {"source": "made up", "dtype": "number", "shape": []},
             },
-            object_keys={'stage': ['x', 'y']},  # The 'x' and 'y' fields above are associated with one device, a 'stage'.
+            object_keys={
+                "stage": ["x", "y"]
+            },  # The 'x' and 'y' fields above are associated with one device, a 'stage'.
             configuration={
-                'stage': {  # The 'stage' has some additional readings which help interpret 'x' and 'y'.
-                    'data_keys': {
-                        'tilt': {'source': 'made up', 'dtype': 'number', 'shape': []},
+                "stage": {  # The 'stage' has some additional readings which help interpret 'x' and 'y'.
+                    "data_keys": {
+                        "tilt": {"source": "made up", "dtype": "number", "shape": []},
                     },
-                    'data': {
-                        'tilt': 5.0
-                    },
-                    'timestamps': {
-                        'tilt': 0.0
-                    }
+                    "data": {"tilt": 5.0},
+                    "timestamps": {"tilt": 0.0},
                 }
-            }
+            },
         )
         builder.add_data(
             "primary",
-            data={'x': [1, 2, 3], 'y': [10, 20, 30]},
-            timestamps={'x': [1, 2, 3], 'y': [10, 20, 30]},
+            data={"x": [1, 2, 3], "y": [10, 20, 30]},
+            timestamps={"x": [1, 2, 3], "y": [10, 20, 30]},
         )
     builder.get_run()
